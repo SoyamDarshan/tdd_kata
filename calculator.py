8444a0b6269e39f39daa6be69b0be8d6a4e47737
SoyamDarshan <32511312+SoyamDarshan@users.noreply.github.com>
2025-02-13 19:14:53 +0530

    - added function to separate delimiter and nums_string - updated default delimiter param to be None - added test case for get_delimiter_nums_string

--- calculator.py
+++ calculator.py
@@ -1,14 +1,25 @@
 import re
 
 
-def parse_string_nums(nums_string, separator=","):
+def parse_string_nums(nums_string, delimiter=None):
+    if not delimiter:
+        delimiter = ","
     if not nums_string:
         return [0]
-    pattern = r'\n|' + separator
+    pattern = r'\n|' + delimiter
     nums_string = [num for num in re.split(pattern, nums_string) if num]
     return list(map(int, nums_string))
 
 
+def get_delimiter_nums_string(nums_string):
+    if not nums_string.startswith(r'//'):
+        return None, nums_string
+    delimiter, numbers = nums_string.split("\n", maxsplit=1)
+    delimiter = "".join(list(delimiter)[2:])
+    return delimiter, numbers
+
+
 def add(nums_string):
-    numbers = parse_string_nums(nums_string)
+    delimiter, nums_string = get_delimiter_nums_string(nums_string)
+    numbers = parse_string_nums(nums_string, delimiter)
     return sum(numbers)
--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from calculator import add, parse_string_nums
+from calculator import add, get_delimiter_nums_string, parse_string_nums
 
 
 class TestCalculator(unittest.TestCase):
@@ -12,6 +12,7 @@ class TestCalculator(unittest.TestCase):
         assert add("1\n2,3") == 6
         assert add("1\n") == 1
         assert add("1\n,\n2\n") == 3
+        assert add("//;\n1;2;3;4") == 10
 
     def test_parse_string_nums(self):
         assert parse_string_nums("") == [0]
@@ -21,6 +22,17 @@ class TestCalculator(unittest.TestCase):
         assert parse_string_nums("1\n2,3") == [1, 2, 3]
         assert parse_string_nums("1\n") == [1]
         assert parse_string_nums("1\n,\n2\n") == [1, 2]
+        assert parse_string_nums("1;\n2;\n3", delimiter=";") == [1, 2, 3]
+
+    def test_get_delimiter_nums_string(self):
+        assert get_delimiter_nums_string("") == (None, "")
+        assert get_delimiter_nums_string("1") == (None, "1")
+        assert get_delimiter_nums_string("1,2") == (None, "1,2")
+        assert get_delimiter_nums_string("1,2,3,4") == (None, "1,2,3,4")
+        assert get_delimiter_nums_string("1\n2,3") == (None, "1\n2,3")
+        assert get_delimiter_nums_string("1\n") == (None, "1\n")
+        assert get_delimiter_nums_string("1\n,\n2\n") == (None, "1\n,\n2\n")
+        assert get_delimiter_nums_string("//;\n1;2;3;4") == (";", "1;2;3;4")
 
 
 if __name__ == '__main__':

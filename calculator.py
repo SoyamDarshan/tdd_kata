5ef596faf2a1254c53ef802c884c4b28c99e07e9
SoyamDarshan <32511312+SoyamDarshan@users.noreply.github.com>
2025-02-13 18:06:22 +0530

    - created parse_string_nums function to handle parsing of strings and return list of numbers - updated add function to perform sum on list of numbers returned from parse_string_nums

--- calculator.py
+++ calculator.py
@@ -1,2 +1,9 @@
-def add(string_nums):
-    return 0
+def parse_string_nums(nums_string):
+    if not nums_string:
+        return [0]
+    return nums_string
+
+
+def add(nums_string):
+    numbers = parse_string_nums(nums_string)
+    return sum(numbers)
--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,14 @@
 import unittest
 
-from calculator import add
+from calculator import add, parse_string_nums
 
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        assert add("") == 0  # add assertion here
+        assert add("") == 0
+
+    def test_parse_string_nums(self):
+        assert parse_string_nums("") == [0]
 
 
 if __name__ == '__main__':
